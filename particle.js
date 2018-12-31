522ac33a08af7eb61ce83dc87547414071413391
adamwynn09 <adam.wynn09@gmail.com>
2018-12-31 19:02:05 +0000

    Added velocity

--- particle.js
+++ particle.js
@@ -1,3 +1,9 @@
+var mass = [];
+var positionX = [];
+var positionY = [];
+var velocityX = [];
+var velocityY = [];
+
 class Particle{
 	constructor(x,y,m,r,g,b,o){
 		this.positionX = x;
@@ -9,10 +15,45 @@ class Particle{
 		this.opacity = o;
 	}
 	
-	draw(){
+	
+	addNewParticle(){
+		mass.push(this.mass);
+		positionX.push(this.positionX);
+	  positionY.push(this.positionY);
+	  velocityX.push(0);
+   	velocityY.push(0);
+		
+		//this will be moved to its own method once DOM working
 		noStroke();
 		fill(this.red, this.green, this.blue, this.opacity);
-		ellipse(this.positionX, this.positionY, this.mass * 1000, this.mass * 1000);
+	}
+	draw(){
+		for (var particleA = 0; particleA < mass.length; particleA++) {
+		var accelerationX = 0, accelerationY = 0;
+		
+		for (var particleB = 0; particleB < mass.length; particleB++) {
+			if (particleA != particleB) {
+				var distanceX = positionX[particleB] - positionX[particleA];
+				var distanceY = positionY[particleB] - positionY[particleA];
+
+				var distance = sqrt(distanceX * distanceX + distanceY * distanceY);
+				if (distance < 1) distance = 1;
+
+				var force = (distance - 320) * mass[particleB] / distance;
+				accelerationX += force * distanceX;
+				accelerationY += force * distanceY;
+			}
+		}
+		
+		velocityX[particleA] = velocityX[particleA] * 0.99 + accelerationX * mass[particleA];
+		velocityY[particleA] = velocityY[particleA] * 0.99 + accelerationY * mass[particleA];
+	}
+	
+	for (var particle = 0; particle < mass.length; particle++) {
+		positionX[particle] += velocityX[particle];
+		positionY[particle] += velocityY[particle];
+		ellipse(positionX[particle], positionY[particle], mass[particle] * 1000, mass[particle] * 1000);
+	}
 	}
 }
 
@@ -22,18 +63,22 @@ function setup(){
 	createCanvas(windowWidth,windowHeight);
 }
 	
-function mouseClicked(){
-	p = new Particle (mouseX, mouseY, random(0.003, 0.03), 64, 255, 255, 192);
+function draw(){
+	background(32);
+	p = new Particle();
 	p.draw();
+}
+
+function mouseClicked(){
+	//colour isnt actually needed here or below
+	p = new Particle (mouseX, mouseY, random(0.003, 0.03), 64, 255, 255, 192);
+	p.addNewParticle();
+	
 }
 
 function mouseDragged(){
 	p = new Particle (mouseX, mouseY, random(0.003, 0.03), 64, 255, 255, 192);
-	p.draw();
-}
-		
-function draw(){
-		background(32);
-		p.draw()
+	p.addNewParticle();
+	
 }
 
