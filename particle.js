65e01a96746161c5c70ceb1023be19a3228369d6
adamwynn09 <adam.wynn09@gmail.com>
2018-12-31 19:11:35 +0000

    Moved p.new... to setup

--- particle.js
+++ particle.js
@@ -61,24 +61,21 @@ var p;
 
 function setup(){
 	createCanvas(windowWidth,windowHeight);
-}
-	
-function draw(){
-	background(32);
-	p = new Particle();
-	p.draw();
+	p = new Particle ();
 }
 
 function mouseClicked(){
-	//colour isnt actually needed here or below
+	//colour isnt actually needed here or below 
 	p = new Particle (mouseX, mouseY, random(0.003, 0.03), 64, 255, 255, 192);
 	p.addNewParticle();
-	
 }
 
 function mouseDragged(){
 	p = new Particle (mouseX, mouseY, random(0.003, 0.03), 64, 255, 255, 192);
 	p.addNewParticle();
-	
 }
-
+	
+function draw(){
+	background(32);
+	p.draw();
+}
