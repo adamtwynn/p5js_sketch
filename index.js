dfcc35bdb9d5a26bb321258986c5340186ec4a59
adamwynn09 <adam.wynn09@gmail.com>
2019-01-10 10:30:50 +0000

    Eslint: Spaces and Quotation Marks

--- index.js
+++ index.js
@@ -1,71 +1,71 @@
 var p;
 
 function setup(){
-	createCanvas(windowWidth,windowHeight);
-	p = new Particle (mouseX,mouseY);
+    createCanvas(windowWidth,windowHeight);
+    p = new Particle (mouseX,mouseY);
 }
 function draw(){
-	background(32);
-	p.draw();
+    background(32);
+    p.draw();
 
 }
 function mouseClicked(){
-	p.addNewParticle();
+    p.addNewParticle();
 }
 
 function mouseDragged(){
-	p.addNewParticle();
+    p.addNewParticle();
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-	var r = document.getElementById("colred");
-	function changeRed(event){
-		let red = document.getElementById("colred").value;
-		p.setRed(red);
-	}
+document.addEventListener('DOMContentLoaded', function(){
+    var r = document.getElementById('colred');
+    function changeRed(event){
+        let red = document.getElementById('colred').value;
+        p.setRed(red);
+    }
 
-	var g = document.getElementById("colgreen");
-	function changeGreen(event){
-		let green = document.getElementById("colgreen").value;
-		p.setGreen(green);
-	}
+    var g = document.getElementById('colgreen');
+    function changeGreen(event){
+        let green = document.getElementById('colgreen').value;
+        p.setGreen(green);
+    }
 
-	var b = document.getElementById("colblue");
-	function changeBlue(event){
-		let blue = document.getElementById("colblue").value;
-		p.setBlue(blue);
-	}
+    var b = document.getElementById('colblue');
+    function changeBlue(event){
+        let blue = document.getElementById('colblue').value;
+        p.setBlue(blue);
+    }
 
-	var reset = document.getElementById("reset");
-	function restart(event){
-		let reset = document.getElementById("reset").value;
-		p.restart(reset);
-	}
+    var reset = document.getElementById('reset');
+    function restart(event){
+        let reset = document.getElementById('reset').value;
+        p.restart(reset);
+    }
 
-	var s = document.getElementById("half");
-	function smaller(event){
-		let small = document.getElementById("half").value;
-		p.smaller(small);
-	}
+    var s = document.getElementById('half');
+    function smaller(event){
+        let small = document.getElementById('half').value;
+        p.smaller(small);
+    }
 
-	var l = document.getElementById("double");
-	function larger(event){
-		let large = document.getElementById("double").value;
-		p.larger(large);
-	}
+    var l = document.getElementById('double');
+    function larger(event){
+        let large = document.getElementById('double').value;
+        p.larger(large);
+    }
 
-	r.addEventListener("change", changeRed);
-	g.addEventListener("change", changeGreen);
-	b.addEventListener("change", changeBlue);
-	reset.addEventListener("click", restart);
-	s.addEventListener("click", smaller);
-	l.addEventListener("click", larger);
-
-
+    r.addEventListener('change', changeRed);
+    g.addEventListener('change', changeGreen);
+    b.addEventListener('change', changeBlue);
+    reset.addEventListener('click', restart);
+    s.addEventListener('click', smaller);
+    l.addEventListener('click', larger);
 
 
-	var cf = document.getElementById("colour_form");
 
-	cf.addEventListener("submit", function (event){
-		event.preventDefault();});
+
+    var cf = document.getElementById('colour_form');
+
+    cf.addEventListener('submit', function (event){
+        event.preventDefault();});
 });
--- particle.js
+++ particle.js
@@ -1,104 +1,104 @@
 class Particle{
-	constructor(x=mouseX,y=mouseY,m=random(0.003,0.03),r=64,g=255,b=255,o=192,s=1000){
-		this.X = x;
-		this.Y = y;
-		this.radius = m;
-		this.red = r;
-		this.green = g;
-		this.blue = b;
-		this.opacity = o;
-		this.size = s;
+    constructor(x=50,y=50,m=random(0.003,0.03),r=64,g=255,b=255,o=192,s=1000){
+        this.X = x;
+        this.Y = y;
+        this.radius = m;
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.opacity = o;
+        this.size = s;
 
-		this.mass = [];
-		this.positionX = [];
-		this.positionY = [];
-		this.velocityX = [];
-		this.velocityY = [];
-	}
-
-
-
-	setRed(red){
-		this.red = red;
-	}
-
-	setGreen(green){
-		this.green = green;
-	}
-
-	setBlue(blue){
-		this.blue = blue;
-	}
-
-	restart(reset){
-		location.reload();
-	}
-
-	larger(large){
-		this.size = this.size*2;
-	}
-	smaller(small){
-		this.size = this.size/2;
-	}
-
-	resetmouseposition(mouseX,mouseY){
-		this.X = mouseX;
-		this.Y = mouseY;
-	}
-
-	addNewParticle(){
-		this.resetmouseposition(mouseX,mouseY);
-		this.radius = random(0.003, 0.03);
-		this.mass.push(this.radius);
-		this.positionX.push(this.X);
-		this.positionY.push(this.Y);
-		this.velocityX.push(0);
-		this.velocityY.push(0);
-	}
-
-	draw(g){
-		if(g){
-			g.fill(this.red, this.green, this.blue, this.opacity);
-			g.noStroke();
-		}
-		else{
-			fill(this.red, this.green, this.blue, this.opacity);
-			noStroke();
-		}
+        this.mass = [];
+        this.positionX = [];
+        this.positionY = [];
+        this.velocityX = [];
+        this.velocityY = [];
+    }
 
 
-		for (this.particleA = 0; this.particleA < this.mass.length; this.particleA++) {
-			this.accelerationX = 0, this.accelerationY = 0;
 
-			for (this.particleB = 0; this.particleB < this.mass.length; this.particleB++) {
-				if (this.particleA != this.particleB) {
-					this.distanceX = this.positionX[this.particleB] - this.positionX[this.particleA];
-					this.distanceY = this.positionY[this.particleB] - this.positionY[this.particleA];
+    setRed(red){
+        this.red = red;
+    }
 
-					this.distance = sqrt(this.distanceX * this.distanceX + this.distanceY * this.distanceY);
-					if (this.distance < 1) this.distance = 1;
+    setGreen(green){
+        this.green = green;
+    }
 
-					this.force = (this.distance - 320) * this.mass[this.particleB] / this.distance;
-					this.accelerationX += this.force * this.distanceX;
-					this.accelerationY += this.force * this.distanceY;
-				}
-			}
+    setBlue(blue){
+        this.blue = blue;
+    }
 
-			this.velocityX[this.particleA] = this.velocityX[this.particleA] * 0.99 + this.accelerationX * this.mass[this.particleA];
-			this.velocityY[this.particleA] = this.velocityY[this.particleA] * 0.99 + this.accelerationY * this.mass[this.particleA];
-		}
+    restart(reset){
+        location.reload();
+    }
 
-		for (this.particle = 0; this.particle < this.mass.length; this.particle++) {
-			this.positionX[this.particle] += this.velocityX[this.particle];
-			this.positionY[this.particle] += this.velocityY[this.particle];
-			if(g){
-				g.ellipse(this.positionX[this.particle], this.positionY[this.particle], this.mass[this.particle] * this.size, this.mass[this.particle] * this.size);
-			}
-			else{
-				ellipse(this.positionX[this.particle], this.positionY[this.particle], this.mass[this.particle] * this.size, this.mass[this.particle] * this.size);
-			}
+    larger(large){
+        this.size = this.size*2;
+    }
+    smaller(small){
+        this.size = this.size/2;
+    }
 
-		}
-	}
+    resetmouseposition(mouseX,mouseY){
+        this.X = mouseX;
+        this.Y = mouseY;
+    }
+
+    addNewParticle(){
+        this.resetmouseposition(mouseX,mouseY);
+        this.radius = random(0.003, 0.03);
+        this.mass.push(this.radius);
+        this.positionX.push(this.X);
+        this.positionY.push(this.Y);
+        this.velocityX.push(0);
+        this.velocityY.push(0);
+    }
+
+    draw(g){
+        if(g){
+            g.fill(this.red, this.green, this.blue, this.opacity);
+            g.noStroke();
+        }
+        else{
+            fill(this.red, this.green, this.blue, this.opacity);
+            noStroke();
+        }
+
+
+        for (this.particleA = 0; this.particleA < this.mass.length; this.particleA++) {
+            this.accelerationX = 0, this.accelerationY = 0;
+
+            for (this.particleB = 0; this.particleB < this.mass.length; this.particleB++) {
+                if (this.particleA != this.particleB) {
+                    this.distanceX = this.positionX[this.particleB] - this.positionX[this.particleA];
+                    this.distanceY = this.positionY[this.particleB] - this.positionY[this.particleA];
+
+                    this.distance = sqrt(this.distanceX * this.distanceX + this.distanceY * this.distanceY);
+                    if (this.distance < 1) this.distance = 1;
+
+                    this.force = (this.distance - 320) * this.mass[this.particleB] / this.distance;
+                    this.accelerationX += this.force * this.distanceX;
+                    this.accelerationY += this.force * this.distanceY;
+                }
+            }
+
+            this.velocityX[this.particleA] = this.velocityX[this.particleA] * 0.99 + this.accelerationX * this.mass[this.particleA];
+            this.velocityY[this.particleA] = this.velocityY[this.particleA] * 0.99 + this.accelerationY * this.mass[this.particleA];
+        }
+
+        for (this.particle = 0; this.particle < this.mass.length; this.particle++) {
+            this.positionX[this.particle] += this.velocityX[this.particle];
+            this.positionY[this.particle] += this.velocityY[this.particle];
+            if(g){
+                g.ellipse(this.positionX[this.particle], this.positionY[this.particle], this.mass[this.particle] * this.size, this.mass[this.particle] * this.size);
+            }
+            else{
+                ellipse(this.positionX[this.particle], this.positionY[this.particle], this.mass[this.particle] * this.size, this.mass[this.particle] * this.size);
+            }
+
+        }
+    }
 
 }
